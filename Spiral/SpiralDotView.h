2dd2a389848017299bf134b420ade85a4b9c9818
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-15 23:03:57 -0700

    first commit

---
+++ Spiral/SpiralDotView.h
@@ -0,0 +1,13 @@
+//
+//  SpiralDotView.h
+//  Spiral
+//
+//  Created by Zakk Hoyt on 5/15/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpiralDotView : UIView
+
+@end
---
+++ Spiral/SpiralDotView.m
@@ -0,0 +1,21 @@
+//
+//  SpiralDotView.m
+//  Spiral
+//
+//  Created by Zakk Hoyt on 5/15/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import "SpiralDotView.h"
+
+@implementation SpiralDotView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
